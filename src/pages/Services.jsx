4ae21a304362347f8f9dca136fa333709e1bab61
RohitStar45 <rohitindi97@gmail.com>
2025-06-26 08:50:34 +0530

    Add files via upload

---
+++ src/pages/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../sections/Footer/Footer';
+import ServicesBanner from '../sections/ServicesBanner/ServicesBanner';
+
+const Services = () => {
+    return (
+        <>
+            <Navbar />
+            <ServicesBanner />
+            <Footer />
+        </>
+    );
+};
+
+export default Services;
